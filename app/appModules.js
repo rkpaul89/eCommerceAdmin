7e8abfcc5005a8b5ba84f10b4ba6e3d8da210bfe
rkpaul89 <rkpaul89@gmail.com>
2020-03-24 16:25:02 +0100

    added ui-pagination module

--- app/appModules.js
+++ app/appModules.js
@@ -4,7 +4,8 @@
     var app=angular.module('app', [
         // Angular modules 
         'ngRoute',
-        'ui.grid'
+        'ui.grid',
+        'ui.grid.pagination'
         // Custom modules 
 
         // 3rd Party Modules
@@ -13,6 +14,7 @@
     app.config(function ($routeProvider, $httpProvider) {
         $routeProvider
             .when("/products/categories", { templateUrl: "app/Products/categories.html", Controller: "productCategories" })
+            .when("/products/addCategory", { templateUrl:"app/Products/addCategory.html" })
             .otherwise({ redirectTo: 'dashboard', templateUrl:"app/dashboard.html" });
     }); 
     //Run block is used to initialize certain values for further use, register global events and anything that needs to run at the beginning 
