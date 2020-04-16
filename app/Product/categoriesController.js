3caa0b05b9efa21cc4822da61dec190c11cf3d83
rkpaul89 <rkpaul89@gmail.com>
2020-04-16 15:36:23 +0200

    changed file name and some other minor modifications

--- app/Product/categoriesController.js
+++ app/Product/categoriesController.js
@@ -1,4 +1,4 @@
-﻿angular.module("app").controller("productCategoriesView", function productCategories($scope, $http, $location, msgAlert,toast) {
+﻿angular.module("app").controller("productCategoriesView", function productCategories($scope, $http, $location, dialog,toast) {
     var vm = $scope;
 
     var getData = function () {
@@ -11,18 +11,25 @@
         enableSorting: true,
         paginationPageSizes: [10, 25, 50,75,100],
         paginationPageSize: 10,
-        enableFiltering:true
-        //columnDefs: [
-        //    { name: 'categoryID', field: 'categoryID' },
-        //    { name: 'categoryName', field: 'categoryName' },
-        //]
+        //enableFiltering:true
+        columnDefs: [
+            { name: 'categoryID', field: 'categoryID' },
+            { name: 'categoryName', field: 'categoryName' },
+            { name: 'Actions', display: 'Actions', cellTemplate: '<i class="fas fa-edit ml-2 mr-3" ng-click="grid.appScope.edit(row.entity)"></i><i class= "fas fa-trash" ng-click="grid.appScope.remove(row.entity)" ></i >' },
+        ]
     };
+    vm.edit = function (entity) {
+        console.log(entity);
+    }
+    vm.remove = function (entity) {
+        var f = dialog.confirm("Are you sure ?");
+    }
     getData();
 });
 
 angular.module("app").controller("productCategoriesCreate", function ($scope,$http,$location,toast) {
     $scope.saveCategory = function () {
-        $http.post("api/productCategories", JSON.stringify(vm.category)).then(function (res) {
+        $http.post("api/productCategories", JSON.stringify($scope.category)).then(function (res) {
             toast.added();
             $location.path("/product/categories");
         });
